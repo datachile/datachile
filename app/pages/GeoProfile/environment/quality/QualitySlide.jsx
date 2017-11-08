f6c9bfb7aacfe1bd8e3f9d2d1eb3a4e9cfe4bb6f
Manuel Aristaran <jazzido@jazzido.com>
2017-11-07 21:53:38 -0300

    trying to implement a barchart

---
+++ app/pages/GeoProfile/environment/quality/QualitySlide.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router";
+import { translate } from "react-i18next";
+import { Section } from "datawheel-canon";
+
+import FeaturedDatum from "components/FeaturedDatum";
+
+class QualitySlide extends Section {
+  static need = [];
+
+  render() {
+    const { children, t } = this.props;
+
+    return (
+      <div className="topic-slide-block">
+        <div className="topic-slide-intro">
+          <div className="topic-slide-title">{t("Quality")}</div>
+          <div className="topic-slide-text">
+            Aliquam erat volutpat. Nunc eleifend leo vitae magna. In id erat non
+            orci commodo lobortis. Proin neque massa, cursus ut, gravida ut,
+            lobortis eget, lacus. Sed diam. Praesent fermentum tempor tellus.
+            Nullam tempus. Mauris ac felis vel velit tristique imperdiet. Donec
+            at pede. Etiam vel neque nec dui dignissim bibendum. Vivamus id
+            enim. Phasellus neque orci, porta a, aliquet quis, semper a, massa.
+            Phasellus purus. Pellentesque tristique imperdiet tortor. Nam
+            euismod tellus id erat.
+          </div>
+          <div className="topic-slide-data">
+            <FeaturedDatum
+              className="lost-1-3"
+              icon="empleo"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+            <FeaturedDatum
+              className="lost-1-3"
+              icon="empleo"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+            <FeaturedDatum
+              className="lost-1-3"
+              icon="industria"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+          </div>
+        </div>
+        <div className="topic-slide-charts">{children}</div>
+      </div>
+    );
+  }
+}
+
+export default translate()(
+  connect(
+    state => ({
+      data: state.data
+    }),
+    {}
+  )(QualitySlide)
+);
