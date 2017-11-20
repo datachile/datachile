ec296654911ac242512d64f36f1371f9fb6eb943
Manuel Aristaran <jazzido@jazzido.com>
2017-11-20 13:10:25 -0300

    LoadingWithProgress

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -9,17 +9,42 @@ const searchActive = (state = false, action) => {
 	}
 };
 
+function loadingProgress(state = { requests: 0, fulfilled: 0 }, action) {
+  switch (action.type) {
+  case "GET_DATA_REQUEST":
+    return {
+      ...state,
+      requests: state.requests + 1
+    };
+  case "GET_DATA_SUCCESS":
+    return {
+      ...state,
+      fulfilled: state.fulfilled + 1
+    };
+  case "LOADING_START":
+  case "LOADING_END":
+    return {
+      requests: 0,
+      fulfilled: 0
+    };
+  }
+  return state;
+}
+
+const id = (state = {}) => state;
+
 export default {
-	focus: (state = {}) => state,
-	population_year: (state = {}) => state,
-	income_year: (state = {}) => state,
-	psu_year: (state = {}) => state,
-	presidential_election_year: (state = {}) => state,
-	senators_election_year: (state = {}) => state,
-	mayor_election_year: (state = {}) => state,
-	exports_year: (state = {}) => state,
-  tax_data_year: (state = {}) => state,
-	nene_year: (state = {}) => state,
-	sources: (state = {}) => state,
+	focus: id,
+	population_year: id,
+	income_year: id,
+	psu_year: id,
+	presidential_election_year: id,
+	senators_election_year: id,
+	mayor_election_year: id,
+	exports_year: id,
+  tax_data_year: id,
+	nene_year: id,
+	sources: id,
+  loadingProgress: loadingProgress,
 	search: combineReducers({ searchActive })
 };
