05c77c4e55d8c9de98e6363ecd3f0919ce2ed0b3
Manuel Aristarán <jazzido@Manuel-Aristarans-Mac-Pro.local>
2017-11-28 14:18:29 -0300

    adjust code style

--- app/pages/CountryProfile/demography/charts/MigrationByEducation.jsx
+++ app/pages/CountryProfile/demography/charts/MigrationByEducation.jsx
@@ -10,82 +10,82 @@ import { numeral } from "helpers/formatters";
 
 import ExportLink from "components/ExportLink";
 
-export default translate()(
-  class MigrationByEducation extends Section {
-    static need = [
-      (params, store) => {
-        const country = getLevelObject(params);
-        const prm = mondrianClient.cube("immigration").then(cube => {
-          const q = levelCut(
-            country,
-            "Origin Country",
-            "Country",
-            cube.query
-              .option("parents", true)
-              .drilldown("Date", "Year")
-              .drilldown("Education", "Education")
-              .measure("Number of visas"),
-            "Subregion",
-            "Country",
-            store.i18n.locale,
-            false
-          );
-
-          return {
-            key: "path_country_migration_by_education",
-            data: store.env.CANON_API + q.path("jsonrecords")
-          };
-        });
+class MigrationByEducation extends Section {
+  static need = [
+    (params, store) => {
+      const country = getLevelObject(params);
+      const prm = mondrianClient.cube("immigration").then(cube => {
+        const q = levelCut(
+          country,
+          "Origin Country",
+          "Country",
+          cube.query
+            .option("parents", true)
+            .drilldown("Date", "Year")
+            .drilldown("Education", "Education")
+            .measure("Number of visas"),
+          "Subregion",
+          "Country",
+          store.i18n.locale,
+          false
+        );
 
         return {
-          type: "GET_DATA",
-          promise: prm
+          key: "path_country_migration_by_education",
+          data: store.env.CANON_API + q.path("jsonrecords")
         };
-      }
-    ];
+      });
 
-    render() {
-      const { t, className, i18n } = this.props;
-
-      if (!i18n.language) return null;
-
-      const locale = i18n.language.split("-")[0];
-
-      const path = this.context.data.path_country_migration_by_education;
-
-      return (
-        <div className={className}>
-          <h3 className="chart-title">
-            <span>{t("Migration By Education")}</span>
-            <ExportLink path={path} />
-          </h3>
-          <Treemap
-            config={{
-              height: 500,
-              data: path,
-              groupBy: "ID Education",
-              label: d => d["Education"],
-              sum: d => d["Number of visas"],
-              time: "ID Year",
-              shapeConfig: {
-                fill: d => ordinalColorScale(d["ID Education"])
-              },
-              tooltipConfig: {
-                title: d => d["Education"],
-                body: d =>
-                  numeral(d["Number of visas"], locale).format("( 0,0 )") +
-                  " " +
-                  t("visas")
-              },
-              legendConfig: {
-                label: false,
-                shapeConfig: false
-              }
-            }}
-            dataFormat={data => data.data}
-          />
-        </div>
-      );
+      return {
+        type: "GET_DATA",
+        promise: prm
+      };
     }
+  ];
+
+  render() {
+    const { t, className, i18n } = this.props;
+
+    if (!i18n.language) return null;
+
+    const locale = i18n.language.split("-")[0];
+
+    const path = this.context.data.path_country_migration_by_education;
+
+    return (
+      <div className={className}>
+        <h3 className="chart-title">
+          <span>{t("Migration By Education")}</span>
+          <ExportLink path={path} />
+        </h3>
+        <Treemap
+          config={{
+            height: 500,
+            data: path,
+            groupBy: "ID Education",
+            label: d => d["Education"],
+            sum: d => d["Number of visas"],
+            time: "ID Year",
+            shapeConfig: {
+              fill: d => ordinalColorScale(d["ID Education"])
+            },
+            tooltipConfig: {
+              title: d => d["Education"],
+              body: d =>
+                numeral(d["Number of visas"], locale).format("( 0,0 )") +
+                " " +
+                t("visas")
+            },
+            legendConfig: {
+              label: false,
+              shapeConfig: false
+            }
+          }}
+          dataFormat={data => data.data}
+        />
+      </div>
+    );
   }
-);
+}
+
+export default translate()(MigrationByEducation);
