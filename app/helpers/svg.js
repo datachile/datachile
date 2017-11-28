84c3123f57e6a2a6824a79af485914385d5ed6ea
Manuel Aristarán <jazzido@Manuel-Aristarans-Mac-Pro.local>
2017-11-28 13:04:49 -0300

    running `prettier` recursively

--- app/helpers/svg.js
+++ app/helpers/svg.js
@@ -1,25 +1,25 @@
 class svgImageCache {
-	constructor() {
-		this.random = Math.random();
-		this.cache = {};
-	}
-	getSvg(path) {
-		return this.cache[path] ? this.cache[path] : false;
-	}
-	setSvg(path, svg) {
-		this.cache[path] = svg;
-	}
+  constructor() {
+    this.random = Math.random();
+    this.cache = {};
+  }
+  getSvg(path) {
+    return this.cache[path] ? this.cache[path] : false;
+  }
+  setSvg(path, svg) {
+    this.cache[path] = svg;
+  }
 }
 
 class SVGImageCacheSingleton {
-	singleton: false;
-	constructor() {}
-	static get instance() {
-		if (!this.singleton) {
-			this.singleton = new svgImageCache();
-		}
-		return this.singleton;
-	}
+  singleton: false;
+  constructor() {}
+  static get instance() {
+    if (!this.singleton) {
+      this.singleton = new svgImageCache();
+    }
+    return this.singleton;
+  }
 }
 
 const SVGCache = SVGImageCacheSingleton;
