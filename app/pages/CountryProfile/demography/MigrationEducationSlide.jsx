5e44d6b35a415093e3866998c850b2d5cf16b16f
Mauricio Vargas <mvargas@dcc.uchile.cl>
2017-11-28 12:17:48 -0300

    adds chart requested in #129

---
+++ app/pages/CountryProfile/demography/MigrationEducationSlide.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router";
+import { translate } from "react-i18next";
+import { Section } from "datawheel-canon";
+
+import FeaturedDatum from "components/FeaturedDatum";
+
+class MigrationEducationSlide extends Section {
+  static need = [];
+
+  render() {
+    const { children, t } = this.props;
+
+    return (
+      <div className="topic-slide-block">
+        <div className="topic-slide-intro">
+          <div className="topic-slide-title">{t("Migration")}</div>
+          <div className="topic-slide-text">
+            <p>Lorem ipsum</p>
+          </div>
+          <div className="topic-slide-data">
+            <FeaturedDatum
+              className="l-1-3"
+              icon="empleo"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+            <FeaturedDatum
+              className="l-1-3"
+              icon="empleo"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+            <FeaturedDatum
+              className="l-1-3"
+              icon="industria"
+              datum="xx"
+              title="Lorem ipsum"
+              subtitle="Lorem blabla"
+            />
+          </div>
+        </div>
+        <div className="topic-slide-charts">{children}</div>
+      </div>
+    );
+  }
+}
+
+export default translate()(
+  connect(
+    state => ({
+      data: state.data
+    }),
+    {}
+  )(MigrationEducationSlide)
+);
