6c3dc9c0a655169869e2d1aef2bfec4239ffd253
Francisco <git.id@frabarz.cl>
2017-12-01 00:39:04 -0300

    Adding styles

--- app/pages/Explore/results/ResultsCountries.jsx
+++ app/pages/Explore/results/ResultsCountries.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import ResultsComponent from "./ResultsGeneric";
 
 class ResultsCountries extends ResultsComponent {
-  entity = "countries";
-
   renderChildrenTitle() {
     return <h4>{this.props.t("Countries")}</h4>;
   }
