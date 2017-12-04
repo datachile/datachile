61645434b86792faacd745c4ec8956f2744bc1df
Manuel Aristarán <jazzido@jazzido.com>
2017-12-04 12:27:32 -0300

    Addresses #109

--- app/pages/GeoProfile/demography/origins/MigrationSlide.jsx
+++ app/pages/GeoProfile/demography/origins/MigrationSlide.jsx
@@ -10,30 +10,25 @@ import { simpleDatumNeed } from "helpers/MondrianClient";
 import { numeral } from "helpers/formatters";
 
 class MigrationSlide extends Section {
-  
   static need = [
-    simpleDatumNeed(
-      "datum_migration_origin",
-      "immigration",
-      ["Number of visas"],
-      {
-        drillDowns: [
-          ["Date", "Date", "Year"]
-        ],
-        options: { parents: false },
-        cuts: [`[Date].[Date].[Year].&[2016]`]
-      }
-    )
+    (params, store) =>
+      simpleDatumNeed(
+        "datum_migration_origin",
+        "immigration",
+        ["Number of visas"],
+        {
+          drillDowns: [["Date", "Date", "Year"]],
+          options: { parents: false },
+          cuts: [`[Date].[Date].[Year].&[${store.sources.immigration.year}]`]
+        }
+      )(params, store)
   ];
 
   render() {
-    const { children, t, i18n } = this.props;
+    const { children, t, i18n, immigration_year } = this.props;
     const { datum_migration_origin } = this.context.data;
 
-    console.log(this.context.data.datum_migration_origin)
-
-    if (!i18n.language) return null;
-    const locale = i18n.language.split("-")[0];
+    const locale = i18n.locale;
 
     return (
       <div className="topic-slide-block">
@@ -53,11 +48,9 @@ class MigrationSlide extends Section {
             <FeaturedDatum
               className="l-1-3"
               icon="empleo"
-              datum={numeral(datum_migration_origin, locale).format(
-                "(0,0)"
-              )}
-              title="Lorem ipsum"
-              subtitle="Lorem blabla"
+              datum={numeral(datum_migration_origin, locale).format("(0,0)")}
+              title={t("Immigrant visas")}
+              subtitle={t("granted in") + " " + immigration_year}
             />
             <FeaturedDatum
               className="l-1-3"
@@ -84,7 +77,8 @@ class MigrationSlide extends Section {
 export default translate()(
   connect(
     state => ({
-      data: state.data
+      data: state.data,
+      immigration_year: state.sources.immigration.year
     }),
     {}
   )(MigrationSlide)
