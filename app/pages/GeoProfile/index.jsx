5ed90dc50be44db8191c7629d10ec6932493bd42
Francisco <git.id@frabarz.cl>
2017-12-05 03:48:42 -0300

    Declaring state object explicitly for chart interactions

--- app/pages/GeoProfile/index.jsx
+++ app/pages/GeoProfile/index.jsx
@@ -285,6 +285,8 @@ class GeoProfile extends Component {
     //MayorResults
   ];
 
+  state = {};
+
   setSharedState = evt => {
     /** This is how this works:
      * The component with the chart needs 3 parameters:
