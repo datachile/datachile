da1a2f1636ef9a0e2bc2390ac6455247eb9d3995
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-12-06 01:02:52 -0300

    Propagation of total in Treemaps

--- app/pages/GeoProfile/economy/innovation/charts/SpendingBySector.jsx
+++ app/pages/GeoProfile/economy/innovation/charts/SpendingBySector.jsx
@@ -7,6 +7,7 @@ import mondrianClient, {
 } from "helpers/MondrianClient";
 import { getGeoObject } from "helpers/dataUtils";
 import { ordinalColorScale } from "helpers/colors";
+import { numeral } from "helpers/formatters";
 import { translate } from "react-i18next";
 import { Section } from "datawheel-canon";
 
@@ -51,7 +52,8 @@ class SpendingBySector extends Section {
 
   render() {
     const path = this.context.data.path_spending_by_sector;
-    const { t, className } = this.props;
+    const { t, className, i18n } = this.props;
+    const locale = i18n.locale;
 
     const geo = this.context.data.geo;
     const regionID = geo.type === "comuna" ? geo.ancestors[0].key : "";
@@ -77,6 +79,12 @@ class SpendingBySector extends Section {
             groupBy: "ID Ownership Type",
             label: d => d["Ownership Type"],
             sum: d => d[measureName],
+            total: d => d[measureName],
+            totalConfig: {
+              text: d =>
+                "Total: US" +
+                numeral(d.text.split(": ")[1], locale).format("$ (0,0)")
+            },
             time: "ID Year",
             shapeConfig: {
               fill: d => ordinalColorScale(d["ID Ownership Type"])
