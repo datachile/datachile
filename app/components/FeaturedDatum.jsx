23431e4f1d99d6d9de0a127b38459e8c98f3e167
Francisco <git.id@frabarz.cl>
2017-12-11 20:28:05 -0300

    Simplifying the FeaturedDatum component

--- app/components/FeaturedDatum.jsx
+++ app/components/FeaturedDatum.jsx
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router";
-import { translate } from "react-i18next";
-import { FORMATTERS } from "helpers/formatters";
+import React from "react";
 import SvgImage from "components/SvgImage";
 
 import "./FeaturedDatum.css";
 
-class FeaturedDatum extends Component {
-  render() {
-    const { t, icon, datum, title, subtitle, className } = this.props;
+function FeaturedDatum(props) {
+  const { icon, datum, title, subtitle, className } = props;
 
-    return (
-      <div className={"featured-datum " + className}>
-        <div className="featured-datum-icon">
-          <SvgImage src={`/images/slider-icon/icon-${icon}.svg`} />
-        </div>
-        <div className="featured-datum-text">
-          <p className="featured-datum-data">{datum}</p>
-          <h4 className="featured-datum-title">{title}</h4>
-          <p className="featured-datum-subtitle">{subtitle}</p>
-        </div>
+  return (
+    <div className={"featured-datum " + className}>
+      <div className="featured-datum-icon">
+        <SvgImage src={`/images/slider-icon/icon-${icon}.svg`} />
       </div>
-    );
-  }
+      <div className="featured-datum-text">
+        <p className="featured-datum-data">{datum}</p>
+        <h4 className="featured-datum-title">{title}</h4>
+        <p className="featured-datum-subtitle">{subtitle}</p>
+      </div>
+    </div>
+  );
 }
 
-export default translate()(connect(state => ({}), {})(FeaturedDatum));
+export default FeaturedDatum;
