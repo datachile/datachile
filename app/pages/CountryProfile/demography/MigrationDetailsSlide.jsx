968794bc5c5ebbcb5c657a525deb26981c88bf4b
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-12-15 00:31:59 -0300

    cleaning and reorder slides

--- app/pages/CountryProfile/demography/MigrationDetailsSlide.jsx
+++ app/pages/CountryProfile/demography/MigrationDetailsSlide.jsx
@@ -12,38 +12,6 @@ class MigrationDetailsSlide extends Section {
 
     return (
       <div className="topic-slide-block">
-        <div className="topic-slide-intro">
-          <div className="topic-slide-title">{t("Migration")}</div>
-          <div
-            className="topic-slide-text"
-            dangerouslySetInnerHTML={{
-              __html: t("country_profile.migration_details_slide.text")
-            }}
-          />
-          <div className="topic-slide-data">
-            <FeaturedDatum
-              className="l-1-3"
-              icon="empleo"
-              datum="xx"
-              title="Lorem ipsum"
-              subtitle="Lorem blabla"
-            />
-            <FeaturedDatum
-              className="l-1-3"
-              icon="empleo"
-              datum="xx"
-              title="Lorem ipsum"
-              subtitle="Lorem blabla"
-            />
-            <FeaturedDatum
-              className="l-1-3"
-              icon="industria"
-              datum="xx"
-              title="Lorem ipsum"
-              subtitle="Lorem blabla"
-            />
-          </div>
-        </div>
         <div className="topic-slide-charts">{children}</div>
       </div>
     );
