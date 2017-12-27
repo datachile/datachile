ad50f8f3c27d5e7fbf5cacd8031c1104fecd83b6
Francisco <git.id@frabarz.cl>
2017-12-27 13:12:53 -0300

    Adding Shiitake to FeaturedDatum

--- app/components/FeaturedDatum.jsx
+++ app/components/FeaturedDatum.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SvgImage from "components/SvgImage";
+import Shiitake from "shiitake";
 
 import "./FeaturedDatum.css";
 
@@ -12,7 +13,9 @@ function FeaturedDatum(props) {
         <SvgImage src={`/images/slider-icon/icon-${icon}.svg`} />
       </div>
       <div className="featured-datum-text">
-        <p className="featured-datum-data">{datum}</p>
+        <Shiitake lines={3} tagName="p" className="featured-datum-data">
+          {datum}
+        </Shiitake>
         <h4 className="featured-datum-title">{title}</h4>
         <p className="featured-datum-subtitle">{subtitle}</p>
       </div>
