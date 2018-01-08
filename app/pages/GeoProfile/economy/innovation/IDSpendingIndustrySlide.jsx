cd80c0532f31951a2ea805d2e2d932f8a86779f0
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2018-01-07 22:23:46 -0300

    cambio I+D

--- app/pages/GeoProfile/economy/innovation/IDSpendingIndustrySlide.jsx
+++ app/pages/GeoProfile/economy/innovation/IDSpendingIndustrySlide.jsx
@@ -131,7 +131,11 @@ class IDSpendingCategorySlide extends Section {
                 icon="empleo"
                 datum={"US " + text.industry.total}
                 title={t("R&D spending")}
-                subtitle={t("Executed by companies in") + " " + sources.rd_survey.last_year}
+                subtitle={
+                  t("Executed by companies in") +
+                  " " +
+                  sources.rd_survey.last_year
+                }
               />
             )}
           </div>
