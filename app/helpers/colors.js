4804f41810910a2247459ece380b94a1ec9d2aea
Pablo H. Paladino <paladinop@gmail.com>
2018-01-09 19:15:59 -0300

    colors + i81n

--- app/helpers/colors.js
+++ app/helpers/colors.js
@@ -51,13 +51,13 @@ export const employmentBySexColorScale = scaleOrdinal().range([
   "#7167B2"
 ]);
 
-export const incomeByAgeColorScale = scaleOrdinal().range(
+export const incomeByAgeColorScale = scaleOrdinal().range([
   "#E4B0AE",
   "#843835",
   "#E56F6A",
   "#1D8CA1",
   "#3FB8C9"
-);
+]);
 
 export const COLORS_MIGRATION_BY_EDUCATION = [
   "#12A29B",
