be96e7d38c964db77688871a78885696e68eac8b
Pablo H. Paladino <paladinop@gmail.com>
2018-01-30 15:07:04 -0300

    de-indicator bar

--- app/components/StagingIndicator.jsx
+++ app/components/StagingIndicator.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 import "./StagingIndicator.css";
 
-export default () =>
-  typeof __STAGING__ !== "undefined" ? (
-    <div className="staging-indicator" />
-  ) : null;
+export default () => {
+  const staging = typeof __STAGING__ !== "undefined" ? true : false;
+  const dev = typeof __DEV__ !== "undefined" ? true : false;
+  const className = staging
+    ? "staging-indicator-staging"
+    : "staging-indicator-dev";
+  if (staging || dev) {
+    return <div className={`staging-indicator ${className}`} />;
+  } else {
+    return null;
+  }
+};
