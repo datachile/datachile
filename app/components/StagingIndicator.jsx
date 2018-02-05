be0fa8da5887c4a03c662ba6ec868ae303957019
Pablo H. Paladino <paladinop@gmail.com>
2018-02-05 19:10:02 -0300

    fixed #485

--- app/components/StagingIndicator.jsx
+++ app/components/StagingIndicator.jsx
@@ -4,9 +4,15 @@ import "./StagingIndicator.css";
 
 export default () => {
   const staging =
-    typeof __STAGING__ !== "undefined" || __STAGING__ !== false ? true : false;
+    typeof __STAGING__ !== "undefined" ||
+    (typeof __STAGING__ == "boolean" && __STAGING__ !== false)
+      ? true
+      : false;
   const dev =
-    typeof __DEV__ !== "undefined" || __DEV__ !== false ? true : false;
+    typeof __DEV__ !== "undefined" ||
+    (typeof __DEV__ == "boolean" && __DEV__ !== false)
+      ? true
+      : false;
   const className = staging
     ? "staging-indicator-staging"
     : "staging-indicator-dev";
