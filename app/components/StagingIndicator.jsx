238afcf9b54f03c2204e82ab72ddd83a364eafe7
Pablo H. Paladino <paladinop@gmail.com>
2018-02-05 14:47:35 -0300

    fixed #485

--- app/components/StagingIndicator.jsx
+++ app/components/StagingIndicator.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import "./StagingIndicator.css";
 
 export default () => {
-  const staging = typeof __STAGING__ !== "undefined" ? true : false;
-  const dev = typeof __DEV__ !== "undefined" ? true : false;
+  const staging =
+    typeof __STAGING__ !== "undefined" || __STAGING__ !== false ? true : false;
+  const dev =
+    typeof __DEV__ !== "undefined" || __DEV__ !== false ? true : false;
   const className = staging
     ? "staging-indicator-staging"
     : "staging-indicator-dev";
