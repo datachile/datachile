aebb36c5d75eb73de3ccb0e19895812562c6c299
Francisco <git.id@frabarz.cl>
2018-02-07 19:15:26 -0300

    Final change, closes #440

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -208,6 +208,7 @@ class Home extends Component {
           <div className="home-content">
             <div className="home-text">
               <div className="l-col">
+                <img src="/images/home/what.png" alt={t("home.what.title")} />
                 <h2>{t("home.what.title")}</h2>
                 <p
                   dangerouslySetInnerHTML={{
@@ -226,6 +227,7 @@ class Home extends Component {
                 />
               </div>
               <div className="r-col">
+                <img src="/images/home/diff.png" alt={t("home.diff.title")} />
                 <h2>{t("home.diff.title")}</h2>
                 <p
                   dangerouslySetInnerHTML={{
