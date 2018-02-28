474de6abfbdc4b19d31fb7f3c3f06387b9f2a59a
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2018-02-28 15:11:24 -0300

    cleaning Documentation component

--- app/pages/Documentation.jsx
+++ app/pages/Documentation.jsx
@@ -14,7 +14,7 @@ class Documentation extends React.Component {
     const { t } = this.props;
     return (
       <div className="documentation">
-        <section>
+        <section className="intro">
           <h3>{t("documentation_api.title1")}</h3>
           <p
             dangerouslySetInnerHTML={{
@@ -88,130 +88,146 @@ class Documentation extends React.Component {
             />
           </div>
         </section>
-        <h3>{t("documentation_api.title2")}</h3>
-        <div className="method">
-          <span className="text">GET</span>{" "}
-          <span className="url">
-            {`https://chilecube.datawheel.us/cubes/{{cube_name}}/aggregate`}
-          </span>
-        </div>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: t("documentation_api.syntax.text1")
-          }}
-        />
-        <table>
-          <thead>
-            <tr>
-              <th className="field">{t("Param")}</th>
-              <th>{t("Type")}</th>
-              <th className="description">{t("Description")}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>drilldown[]</td>
-              <td>
-                <span className="code">string</span>
-              </td>
-              <td className="description">
-                Para generar un drilldown, se debe tener en cuenta: Dimension,
-                Hierarchy, Level.
-                <span className="code format">
-                  [Dimension].[Hierarchy].[Level]
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>measures[]</td>
-              <td>
-                <span className="code">string</span>
-              </td>
-              <td className="description">
-                <span className="code">Measure</span>
-              </td>
-            </tr>
-            <tr>
-              <td>cut[]</td>
-              <td>
-                <span className="code">string</span>
-              </td>
-              <td className="description">
-                Se debe concatenar el drilldown sobre el cuál se desea hacer el
-                corte, con <span className="code">Level ID</span>.
-                <span className="code format">
-                  [Dimension].[Hierarchy].[Level].&[member_id]<sup>1</sup>
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>nonempty</td>
-              <td>
-                <span className="code">boolean</span>
-              </td>
-              <td className="description">
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: t("documentation_api.definitions.nonempty")
-                  }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>distinct</td>
-              <td>
-                <span className="code">boolean</span>
-              </td>
-              <td className="description">
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: t("documentation_api.definitions.distinct")
-                  }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>parents</td>
-              <td>
-                <span className="code">boolean</span>
-              </td>
-              <td className="description">
-                En caso de encontrarse en un nivel de profundidad mayor a 1,
-                obtiene el ID y nombre de los niveles padres. --
-              </td>
-            </tr>
-            <tr>
-              <td>sparse</td>
-              <td>
-                <span className="code">boolean</span>
-              </td>
-              <td className="description">--</td>
-            </tr>
-          </tbody>
-        </table>
+        <section className="syntax">
+          <h3>{t("documentation_api.title2")}</h3>
 
-        <div className="pagenote">
-          1. Si se hace la consulta directamente en URL, se debe reemplazar &
-          por %26 para evitar problemas de parseo del corte.
-        </div>
+          <div className="method">
+            <span className="text">GET</span>{" "}
+            <span className="url">{`/cubes/{{cube_name}}/aggregate`}</span>
+          </div>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("documentation_api.syntax.text1")
+            }}
+          />
+          <table>
+            <thead>
+              <tr>
+                <th className="field">{t("Param")}</th>
+                <th>{t("Type")}</th>
+                <th className="description">{t("Description")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>drilldown[]</td>
+                <td>
+                  <span className="code">string</span>
+                </td>
+                <td className="description">
+                  Para generar un drilldown, se debe tener en cuenta: Dimension,
+                  Hierarchy, Level.
+                  <span className="code format">
+                    [Dimension].[Hierarchy].[Level]
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>measures[]</td>
+                <td>
+                  <span className="code">string</span>
+                </td>
+                <td className="description">
+                  <span className="code">Measure</span>
+                </td>
+              </tr>
+              <tr>
+                <td>cut[]</td>
+                <td>
+                  <span className="code">string</span>
+                </td>
+                <td className="description">
+                  Se debe concatenar el drilldown sobre el cuál se desea hacer
+                  el corte, con <span className="code">Level ID</span>.
+                  <span className="code format">
+                    [Dimension].[Hierarchy].[Level].&[member_id]<sup>1</sup>
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td>nonempty</td>
+                <td>
+                  <span className="code">boolean</span>
+                </td>
+                <td className="description">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: t("documentation_api.definitions.nonempty")
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>distinct</td>
+                <td>
+                  <span className="code">boolean</span>
+                </td>
+                <td className="description">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: t("documentation_api.definitions.distinct")
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>parents</td>
+                <td>
+                  <span className="code">boolean</span>
+                </td>
+                <td className="description">
+                  En caso de encontrarse en un nivel de profundidad mayor a 1,
+                  obtiene el ID y nombre de los niveles padres. --
+                </td>
+              </tr>
+              <tr>
+                <td>sparse</td>
+                <td>
+                  <span className="code">boolean</span>
+                </td>
+                <td className="description">--</td>
+              </tr>
+            </tbody>
+          </table>
 
-        <h3>{t("documentation_api.title3")}</h3>
-        <Datasets />
+          <div className="pagenote">
+            1. Si se hace la consulta directamente en URL, se debe reemplazar &
+            por %26 para evitar problemas de parseo del corte.
+          </div>
+        </section>
+        <section className="syntax">
+          <div className="method">
+            <span className="text">GET</span>{" "}
+            <span className="url">
+              {`/cubes/{{cube_name}}/dimensions/{{dimension_name}}/levels/{{level_name}}/members`}
+            </span>
+          </div>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("documentation_api.syntax.text2")
+            }}
+          />
+        </section>
+        <section className="datasets">
+          <h3>{t("documentation_api.title7")}</h3>
+          <Datasets />
+        </section>
 
-        <h3>{t("documentation_api.title3")}</h3>
-        <div className="def-text">
-          The following examples illustrate how queries are constructed, by
-          restricting the search space with the drilldown, cut, and measures
-          parameters. Aquí se muestra el aspecto de la URL y el JSON devuelto
-          cuando se pregunta ¿Qué número de personas está en cada previsión de
-          salud en Concepción?
-        </div>
-        <div className="code left">
-          {`https://chilecube.datawheel.us//cubes/casen_health_system/aggregate.json?drilldown[]=[Health System].[Health System]&cut[]=[Geography].[Comuna].%26[64]&measures[]=Expansion Factor Comuna&caption[]=[Health System].[Health System].Description ES&caption[]=[Health System].[Health System Group].Description ES&nonempty=true&distinct=false&parents=true&sparse=true`}
-        </div>
-        <pre>
-          <code>
-            {`
+        <section className="examples">
+          <h3>{t("documentation_api.title3")}</h3>
+          <div className="def-text">
+            The following examples illustrate how queries are constructed, by
+            restricting the search space with the drilldown, cut, and measures
+            parameters. Aquí se muestra el aspecto de la URL y el JSON devuelto
+            cuando se pregunta ¿Qué número de personas está en cada previsión de
+            salud en Concepción?
+          </div>
+          <div className="code left">
+            {`https://chilecube.datawheel.us//cubes/casen_health_system/aggregate.json?drilldown[]=[Health System].[Health System]&cut[]=[Geography].[Comuna].%26[64]&measures[]=Expansion Factor Comuna&caption[]=[Health System].[Health System].Description ES&caption[]=[Health System].[Health System Group].Description ES&nonempty=true&distinct=false&parents=true&sparse=true`}
+          </div>
+          <pre>
+            <code>
+              {`
     {
       "axes": [
         {
@@ -269,80 +285,100 @@ class Documentation extends React.Component {
       ]
     }
                 `}
-          </code>
-        </pre>
-        <div className="def-text">
-          <h3>{t("documentation_api.title4")}</h3>
-          These criteria can be combined to define robust queries. Here are a
-          few examples.
-          <div className="example">
-            <p className="text">
-              Seleccionar los valores de las regiones de Valparaíso (ID 5) y de
-              Coquimbo (ID 4)
-            </p>
-            <div className="code">
-              {`cut[]: {[Geography].[Geography].[Region].&[4], [Geography].[Geography].[Region].&[5]}`}
+            </code>
+          </pre>
+        </section>
+
+        <section className="complex">
+          <div className="def-text">
+            <h3>{t("documentation_api.title4")}</h3>
+            These criteria can be combined to define robust queries. Here are a
+            few examples.
+            <div className="example">
+              <p className="text">
+                Seleccionar los valores de las regiones de Valparaíso (ID 5) y
+                de Coquimbo (ID 4)
+              </p>
+              <div className="code">
+                {`cut[]: {[Geography].[Geography].[Region].&[4], [Geography].[Geography].[Region].&[5]}`}
+              </div>
+              <p className="clarification">
+                Para realizar cortes por más de un ID, cada cut debe estar
+                separado por <span className="code">,</span>
+                dentro de <span className="code">{`{}`}</span>.
+              </p>
             </div>
-            <p className="clarification">
-              Para realizar cortes por más de un ID, cada cut debe estar
-              separado por <span className="code">,</span>
-              dentro de <span className="code">{`{}`}</span>.
-            </p>
-          </div>
-          <div className="example">
-            <p className="text">
-              Obtener las importaciones de todas las comunas de Chile entre los
-              años 2013 y 2015.
-            </p>
-            <div className="code">
-              {`drilldown[]: [Origin Country].[Country].[Country]`}
-              <br />
-              {`drilldown[]: [Geography].[Geography].[Comuna]`}
-              <br />
-              {`drilldown[]: [Date].[Date].[Year]`}
-              <br />
-              {`measures[]: CIF US`}
-              <br />
-              {`cut[]: {[Date].[Date].[Year].&[2013], [Date].[Date].[Year].&[2014], [Date].[Date].[Year].&[2015]}`}
+            <div className="example">
+              <p className="text">
+                Obtener las importaciones de todas las comunas de Chile entre
+                los años 2013 y 2015.
+              </p>
+              <div className="code">
+                {`drilldown[]: [Origin Country].[Country].[Country]`}
+                <br />
+                {`drilldown[]: [Geography].[Geography].[Comuna]`}
+                <br />
+                {`drilldown[]: [Date].[Date].[Year]`}
+                <br />
+                {`measures[]: CIF US`}
+                <br />
+                {`cut[]: {[Date].[Date].[Year].&[2013], [Date].[Date].[Year].&[2014], [Date].[Date].[Year].&[2015]}`}
+              </div>
+              <p className="clarification">
+                Para realizar cortes por más de un ID, cada cut debe estar
+                separado por <span className="code">,</span>
+                dentro de <span className="code">{`{}`}</span>.
+              </p>
             </div>
-            <p className="clarification">
-              Para realizar cortes por más de un ID, cada cut debe estar
-              separado por <span className="code">,</span>
-              dentro de <span className="code">{`{}`}</span>.
-            </p>
           </div>
-        </div>
-        <h3>{t("documentation_api.title5")}</h3>
-        <p>https://github.com/Datawheel/mondrian-rest-client</p>
-        <h3>{t("documentation_api.title6")}</h3>
-        <p>
-          Para fomentar el uso de la API de DataChile, hemos habilitado en
-          Python un módulo que permite interactuar con todos los datos
-          disponibles.
-        </p>
-        <p>
-          Para instalar, debes usar:{" "}
-          <span className="code">pip install datachile</span>
-        </p>
-        <pre>
-          <code>
-            {`
-            from opendata_rest.datachile import DataChile 
-            
-            q = DataChile.get(
-                "exports", { 
-                    drilldowns: [
-                        ["Date", "Year"], 
-                        ["Destination Country", "Country", "Continent"]
-                    ], 
-                    measures: ["FOB US", "Geo Rank"],
+        </section>
+
+        <section className="mondrian">
+          <h3>{t("documentation_api.title5")}</h3>
+          <p>https://github.com/Datawheel/mondrian-rest-client</p>
+        </section>
+
+        <section className="client-python">
+          <h3>{t("documentation_api.title6")}</h3>
+          <p>
+            Para fomentar el uso de la API de DataChile, hemos habilitado en
+            Python un módulo que permite interactuar con todos los datos
+            disponibles.
+          </p>
+          <p>
+            Para instalar, debes usar:{" "}
+            <span className="code">pip install datachile</span>
+          </p>
+          <pre>
+            <code>
+              {`
+    from datachile import ChileCube
+
+    client = ChileCube()
+
+    query = client.get(
+        "exports", 
+        {
+            "drilldowns": [
+                ["Date", "Year"],
+                ["Destination Country", "Country", "Country"]
+            ],
+            "measures": ["FOB US"],
+            "cuts": [
+                {
+                    "drilldown": ["Date", "Year"],
+                    "values": [2012, 2013, 2014]
                 }
-            ) 
-            
-            print(q)
+            ],
+            "parents": True
+        }
+    )
+
+    print(query)
             `}
-          </code>
-        </pre>
+            </code>
+          </pre>
+        </section>
       </div>
     );
   }
