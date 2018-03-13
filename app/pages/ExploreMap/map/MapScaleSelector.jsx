e3470efa267f68be0479af96ecb5efdd3c9dc548
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2018-03-13 18:14:22 -0300

    minor changes in ScaleSelector

--- app/pages/ExploreMap/map/MapScaleSelector.jsx
+++ app/pages/ExploreMap/map/MapScaleSelector.jsx
@@ -5,65 +5,59 @@ import { connect } from "react-redux";
 import "./MapScaleSelector.css";
 
 class MapScaleSelector extends React.Component {
-  componentWillReceiveProps(nextProps) {
-    const { setMapScale, queryLog } = nextProps;
+	componentWillReceiveProps(nextProps) {
+		const { setMapScale, queryLog } = nextProps;
 
-    if (!queryLog) {
-      setMapScale("linear");
-    }
-  }
+		if (!queryLog) {
+			setMapScale("linear");
+		}
+	}
 
-  render() {
-    const { t, mapScale, setMapScale, queryLinear, queryLog } = this.props;
+	render() {
+		const { t, mapScale, setMapScale, queryLinear, queryLog } = this.props;
 
-    if (!queryLinear) {
-      return null;
-    }
+		if (!queryLinear) {
+			return null;
+		}
 
-    return (
-      <div className="map-select-options">
-        <p className="map-select-text">{t("Visualize by")}</p>
-        <div className="map-select-options-container">
-          <a
-            className={`toggle ${mapScale === "linear" ? "selected" : ""}`}
-            onClick={evt => setMapScale("linear")}
-          >
-            {t("Linear")}
-          </a>
-          <a
-            className={`toggle ${mapScale === "log" ? "selected" : ""} ${
-              !queryLog ? "disabled" : ""
-            }`}
-            onClick={evt => {
-              if (queryLog) {
-                setMapScale("log");
-              }
-            }}
-          >
-            {t("Log")}
-          </a>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className="map-select-options">
+				<p className="map-select-text">{t("Visualize by")}</p>
+				<div className="map-select-options-container">
+					<a
+						className={`toggle ${mapScale === "linear" ? "selected" : ""}`}
+						onClick={evt => setMapScale("linear")}
+					>
+						{t("Linear")}
+					</a>
+					<a
+						className={`toggle ${mapScale === "log" ? "selected" : ""}`}
+						onClick={evt => setMapScale("log")}
+					>
+						{t("Log")}
+					</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => ({
-  setMapScale(payload) {
-    dispatch({ type: "MAP_SCALE_SET", payload });
-  }
+	setMapScale(payload) {
+		dispatch({ type: "MAP_SCALE_SET", payload });
+	}
 });
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    mapScale: state.map.params.scale,
-    queryLinear: state.map.results.queries.region,
-    queryLog: state.map.results.queries.comuna
-  };
+	return {
+		mapScale: state.map.params.scale,
+		queryLinear: state.map.results.queries.region,
+		queryLog: state.map.results.queries.comuna
+	};
 };
 
 MapScaleSelector = translate()(
-  connect(mapStateToProps, mapDispatchToProps)(MapScaleSelector)
+	connect(mapStateToProps, mapDispatchToProps)(MapScaleSelector)
 );
 
 export default MapScaleSelector;
