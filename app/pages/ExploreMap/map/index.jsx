e9b89e7380c1bb4cb6d568470a60054495215dfb
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2018-03-16 13:12:49 -0300

    first iteration in introslider

--- app/pages/ExploreMap/map/index.jsx
+++ app/pages/ExploreMap/map/index.jsx
@@ -13,14 +13,16 @@ import MapContent from "./MapContent";
 import MapLevelSelector from "./MapLevelSelector";
 import MapOptions from "./MapOptions";
 
+import IntroSlider from "components/IntroSlider";
+
 import { requestData, requestMembers } from "../actions.js";
 
 import { SyncStateAndLocalStorage } from "helpers/localStorage";
 import {
-  getCutsFullName,
-  stateToPermalink,
-  permalinkToState,
-  cutStateParser
+	getCutsFullName,
+	stateToPermalink,
+	permalinkToState,
+	cutStateParser
 } from "helpers/map";
 import mondrianClient from "helpers/MondrianClient";
 import shorthash from "helpers/shorthash";
@@ -31,240 +33,241 @@ import DatachileProgressBar from "components/DatachileProgressBar";
 import "../explore-map.css";
 
 class ExploreMap extends React.Component {
-  static need = [
-    (params, store) => {
-      const hasGeoDimensions = dimensions =>
-        dimensions.length > 0 &&
-        dimensions.some(
-          dim =>
-            dim.hierarchies.length > 0 &&
-            dim.hierarchies.some(hie => hie.name == "Geography")
-        );
+	static need = [
+		(params, store) => {
+			const hasGeoDimensions = dimensions =>
+				dimensions.length > 0 &&
+				dimensions.some(
+					dim =>
+						dim.hierarchies.length > 0 &&
+						dim.hierarchies.some(hie => hie.name == "Geography")
+				);
 
-      const localeCaption = function(key, item) {
-        return item.annotations[key] || item.caption || item.name;
-      }.bind(null, `${store.i18n.locale}_element_caption`);
+			const localeCaption = function(key, item) {
+				return item.annotations[key] || item.caption || item.name;
+			}.bind(null, `${store.i18n.locale}_element_caption`);
 
-      // mondrian-rest-client doesn't use the annotations from the json
-      const promise = mondrianClient.cubes().then(cubes => {
-        const hierarchies = {};
-        const measures = {};
+			// mondrian-rest-client doesn't use the annotations from the json
+			const promise = mondrianClient.cubes().then(cubes => {
+				const hierarchies = {};
+				const measures = {};
 
-        for (let cube, i = 0; (cube = cubes[i]); i++) {
-          if (!hasGeoDimensions(cube.dimensions)) continue;
+				for (let cube, i = 0; (cube = cubes[i]); i++) {
+					if (!hasGeoDimensions(cube.dimensions)) continue;
 
-          const topic = cube.annotations.topic;
-          const availableMs = cube.annotations.available_measures
-            ? cube.annotations.available_measures.split(",")
-            : [];
+					const topic = cube.annotations.topic;
+					const availableMs = cube.annotations.available_measures
+						? cube.annotations.available_measures.split(",")
+						: [];
 
-          measures[topic] = [].concat(
-            measures[topic] || [],
-            cube.measures
-              .filter(ms => availableMs.indexOf(ms.name) > -1)
-              .map(ms => ({
-                hash: shorthash(ms.name),
-                cube: cube.name,
-                value: ms.name,
-                name: localeCaption(ms)
-              }))
-          );
+					measures[topic] = [].concat(
+						measures[topic] || [],
+						cube.measures
+							.filter(ms => availableMs.indexOf(ms.name) > -1)
+							.map(ms => ({
+								hash: shorthash(ms.name),
+								cube: cube.name,
+								value: ms.name,
+								name: localeCaption(ms)
+							}))
+					);
 
-          let selectors = [];
+					let selectors = [];
 
-          const availableDims = cube.annotations.available_dimensions
-            ? cube.annotations.available_dimensions.split(",")
-            : [];
+					const availableDims = cube.annotations.available_dimensions
+						? cube.annotations.available_dimensions.split(",")
+						: [];
 
-          for (let dim, j = 0; (dim = cube.dimensions[j]); j++) {
-            if (
-              !/Geography$|^Date$/.test(dim.name) &&
-              availableDims.indexOf(dim.name) > -1
-            ) {
-              for (let hier, k = 0; (hier = dim.hierarchies[k]); k++) {
-                selectors.push({
-                  hash: shorthash(`[${dim.name}].[${hier.name}]`),
-                  cube: cube.name,
-                  name: localeCaption(dim),
-                  value: `[${dim.name}].[${hier.name}]`,
-                  isGeo: /country/i.test(dim.name),
-                  levels: hier.levels.slice(1).map(lvl => ({
-                    hash: shorthash(lvl.name),
-                    value: lvl.fullName,
-                    name: localeCaption(lvl)
-                  }))
-                });
-              }
-            }
-          }
+					for (let dim, j = 0; (dim = cube.dimensions[j]); j++) {
+						if (
+							!/Geography$|^Date$/.test(dim.name) &&
+							availableDims.indexOf(dim.name) > -1
+						) {
+							for (let hier, k = 0; (hier = dim.hierarchies[k]); k++) {
+								selectors.push({
+									hash: shorthash(`[${dim.name}].[${hier.name}]`),
+									cube: cube.name,
+									name: localeCaption(dim),
+									value: `[${dim.name}].[${hier.name}]`,
+									isGeo: /country/i.test(dim.name),
+									levels: hier.levels.slice(1).map(lvl => ({
+										hash: shorthash(lvl.name),
+										value: lvl.fullName,
+										name: localeCaption(lvl)
+									}))
+								});
+							}
+						}
+					}
 
-          hierarchies[cube.name] = selectors;
-        }
+					hierarchies[cube.name] = selectors;
+				}
 
-        return {
-          key: "map_params",
-          data: {
-            selectors: hierarchies,
-            measures: measures
-          }
-        };
-      });
+				return {
+					key: "map_params",
+					data: {
+						selectors: hierarchies,
+						measures: measures
+					}
+				};
+			});
 
-      return { type: "GET_DATA", promise };
-    }
-  ];
+			return { type: "GET_DATA", promise };
+		}
+	];
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    console.log("ExploreMap is being created");
+		console.log("ExploreMap is being created");
 
-    const t = props.t;
+		const t = props.t;
 
-    const topics = [
-      { value: "economy", name: t("Economy") },
-      { value: "education", name: t("Education") },
-      { value: "environment", name: t("Housing & Environment") },
-      { value: "demography", name: t("Demography") },
-      { value: "health", name: t("Health") },
-      { value: "civics", name: t("Civics") }
-    ].map(item => {
-      item.hash = shorthash(item.value);
-      item.icon = `/images/profile-icon/icon-${item.value}.svg`;
-      return item;
+		const topics = [
+			{ value: "economy", name: t("Economy") },
+			{ value: "education", name: t("Education") },
+			{ value: "environment", name: t("Housing & Environment") },
+			{ value: "demography", name: t("Demography") },
+			{ value: "health", name: t("Health") },
+			{ value: "civics", name: t("Civics") }
+		].map(item => {
+			item.hash = shorthash(item.value);
+			item.icon = `/images/profile-icon/icon-${item.value}.svg`;
+			return item;
+		});
+
+		props.dispatch({
+			type: "MAP_INIT",
+			payload: {
+				topics: topics,
+				params: permalinkToState(
+					props.location.query,
+					topics,
+					props.data.map_params.measures
+				)
+			}
     });
+    
+		this.state = {
+			cutHash: props.location.query.c
+		};
+	}
 
-    props.dispatch({
-      type: "MAP_INIT",
-      payload: {
-        topics: topics,
-        params: permalinkToState(
-          props.location.query,
-          topics,
-          props.data.map_params.measures
-        )
-      }
-    });
+	componentDidMount() {
+		console.log("ExploreMap was mounted");
+	}
 
-    this.state = {
-      cutHash: props.location.query.c
-    };
-  }
+	componentWillUnmount() {
+		console.log("ExploreMap will be unmounted");
+	}
 
-  componentDidMount() {
-    console.log("ExploreMap was mounted");
-  }
+	componentWillReceiveProps(nextProps) {
+		const { dispatch } = this.props;
+		const { mapMemberCubes, mapCube, mapParams } = nextProps;
+		const locale = nextProps.i18n.language;
 
-  componentWillUnmount() {
-    console.log("ExploreMap will be unmounted");
-  }
+		if (
+			this.state.cutHash &&
+			Object.keys(nextProps.mapLevelMembers).length > 0
+		) {
+			this.setState({ cutHash: null });
+			dispatch({
+				type: "MAP_INIT_DEFERRED",
+				payload: cutStateParser(this.state.cutHash, {
+					cube: nextProps.mapCube,
+					selectors: nextProps.data.map_params.selectors,
+					members: nextProps.mapLevelMembers
+				})
+			});
+		}
 
-  componentWillReceiveProps(nextProps) {
-    const { dispatch } = this.props;
-    const { mapMemberCubes, mapCube, mapParams } = nextProps;
-    const locale = nextProps.i18n.language;
+		const mapCubeChanged = mapCube && this.props.mapCube != mapCube;
 
-    if (
-      this.state.cutHash &&
-      Object.keys(nextProps.mapLevelMembers).length > 0
-    ) {
-      this.setState({ cutHash: null });
-      dispatch({
-        type: "MAP_INIT_DEFERRED",
-        payload: cutStateParser(this.state.cutHash, {
-          cube: nextProps.mapCube,
-          selectors: nextProps.data.map_params.selectors,
-          members: nextProps.mapLevelMembers
-        })
-      });
-    }
+		const cutsBefore = getCutsFullName(this.props.mapCuts);
+		const cutsAfter = getCutsFullName(nextProps.mapCuts);
 
-    const mapCubeChanged = mapCube && this.props.mapCube != mapCube;
+		if (mapCubeChanged || !isEqual(cutsBefore, cutsAfter)) {
+			dispatch(
+				requestData({
+					cubeName: mapCube,
+					cuts: cutsAfter,
+					locale: locale
+				})
+			);
+		}
 
-    const cutsBefore = getCutsFullName(this.props.mapCuts);
-    const cutsAfter = getCutsFullName(nextProps.mapCuts);
+		if (mapCubeChanged && !mapMemberCubes.includes(mapCube))
+			dispatch(requestMembers(mapCube, locale));
 
-    if (mapCubeChanged || !isEqual(cutsBefore, cutsAfter)) {
-      dispatch(
-        requestData({
-          cubeName: mapCube,
-          cuts: cutsAfter,
-          locale: locale
-        })
-      );
-    }
+		const permalinkBefore = stateToPermalink(this.props.mapParams);
+		const permalinkAfter = stateToPermalink(nextProps.mapParams);
 
-    if (mapCubeChanged && !mapMemberCubes.includes(mapCube))
-      dispatch(requestMembers(mapCube, locale));
+		if (!isEqual(permalinkBefore, permalinkAfter))
+			browserHistory.push(this.props.location.pathname + permalinkAfter);
+	}
 
-    const permalinkBefore = stateToPermalink(this.props.mapParams);
-    const permalinkAfter = stateToPermalink(nextProps.mapParams);
+	render() {
+		const { section } = this.props.routeParams;
+		const { data, t, membersLoading } = this.props;
 
-    if (!isEqual(permalinkBefore, permalinkAfter))
-      browserHistory.push(this.props.location.pathname + permalinkAfter);
-  }
+		let loadingValue = 0;
+		loadingValue += Number(this.props.statusData == "LOADING");
+		loadingValue += this.props.membersLoaded;
+		const loading = loadingValue > 0 || this.state.cutHash ? "loading" : "";
 
-  render() {
-    const { section } = this.props.routeParams;
-    const { data, t, membersLoading } = this.props;
+		return (
+			<Canon>
+				<CanonProfile id="explore-map" data={data} topics={[]}>
+					{this.state.gettingStarted && <IntroSlider />}
+					<div className="explore-map-page">
+						<Nav title="" typeTitle="" type={false} dark={true} />
 
-    let loadingValue = 0;
-    loadingValue += Number(this.props.statusData == "LOADING");
-    loadingValue += this.props.membersLoaded;
-    const loading = loadingValue > 0 || this.state.cutHash ? "loading" : "";
-
-    return (
-      <Canon>
-        <CanonProfile id="explore-map" data={data} topics={[]}>
-          <div className="explore-map-page">
-            <Nav title="" typeTitle="" type={false} dark={true} />
-
-            <div className="explore-map-container">
-              <div className="explore-map-section">
-                <div className="explore-map-sidebar">
-                  <MapSidebar data={data} />
-                </div>
-                <div className="explore-map-content">
-                  <NonIdealState
-                    className={`explore-map-loading ${loading}`}
-                    title={t("loading.map")}
-                    description={t("loading.developed")}
-                    visual={
-                      <DatachileProgressBar
-                        value={loadingValue / (membersLoading + 1)}
-                      />
-                    }
-                  />
-                  <div className="map-options-row">
-                    <MapLevelSelector />
-                    <MapOptions />
-                  </div>
-                  <MapContent />
-                </div>
-              </div>
-            </div>
-          </div>
-        </CanonProfile>
-      </Canon>
-    );
-  }
+						<div className="explore-map-container">
+							<div className="explore-map-section">
+								<div className="explore-map-sidebar">
+									<MapSidebar data={data} />
+								</div>
+								<div className="explore-map-content">
+									<NonIdealState
+										className={`explore-map-loading ${loading}`}
+										title={t("loading.map")}
+										description={t("loading.developed")}
+										visual={
+											<DatachileProgressBar
+												value={loadingValue / (membersLoading + 1)}
+											/>
+										}
+									/>
+									<div className="map-options-row">
+										<MapLevelSelector />
+										<MapOptions />
+									</div>
+									<MapContent />
+								</div>
+							</div>
+						</div>
+					</div>
+				</CanonProfile>
+			</Canon>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  const params = state.map.params;
-  return {
-    data: state.data,
+	const params = state.map.params;
+	return {
+		data: state.data,
 
-    statusData: state.map.results.status,
-    membersLoading: state.map.options.countLoading,
-    membersLoaded: state.map.options.countLoaded,
+		statusData: state.map.results.status,
+		membersLoading: state.map.options.countLoading,
+		membersLoaded: state.map.options.countLoaded,
 
-    mapMemberCubes: state.map.options.cubes,
-    mapLevelMembers: state.map.options.members,
-    mapParams: params,
-    mapCube: params.measure && params.measure.cube,
-    mapCuts: params.cuts
-  };
+		mapMemberCubes: state.map.options.cubes,
+		mapLevelMembers: state.map.options.members,
+		mapParams: params,
+		mapCube: params.measure && params.measure.cube,
+		mapCuts: params.cuts
+	};
 };
 
 export default translate()(connect(mapStateToProps)(ExploreMap));
