d877fdfff7f151a2f45526bb2f880ed2efcd3612
Francisco <git.id@frabarz.cl>
2018-04-16 03:46:09 -0300

    Mayor slide

--- app/pages/GeoProfile/civics/election/MayorSlide.jsx
+++ app/pages/GeoProfile/civics/election/MayorSlide.jsx
@@ -4,49 +4,72 @@ import { Section } from "datawheel-canon";
 
 import { simpleDatumNeed } from "helpers/MondrianClient";
 import { numeral } from "helpers/formatters";
+import { sources } from "helpers/consts";
 
 import FeaturedDatum from "components/FeaturedDatum";
 
-import { Election } from "texts/GeoProfile";
+import { textCivicsMayor } from "texts/GeoProfile";
+
+const election_year = []
+  .concat(sources.election_results_update.mayor_election_year)
+  .pop();
 
 class MayorSlide extends Section {
   static need = [
-    (params, store) =>
-      simpleDatumNeed(
-        "datum_electoral_participation",
-        "election_participation",
-        ["Votes", "Participation"],
-        {
-          drillDowns: [
-            ["Election Type", "Election Type", "Election Type"],
-            ["Date", "Date", "Year"]
-          ],
-          options: { parents: true },
-          cuts: ["[Date].[Date].[Year].&[2017]"]
-        },
-        "geo",
-        false
-      )(params, store)
+    simpleDatumNeed(
+      "datum_election_mayor",
+      "election_results_update",
+      ["Number of records", "Votes"],
+      {
+        drillDowns: [
+          ["Party", "Party", "Partido"],
+          ["Candidates", "Candidates", "Candidate"],
+          ["Elected", "Elected", "Elected"]
+        ],
+        options: { sparse: true },
+        cuts: [
+          "[Election Type].[Election Type].[Election Type].&[5]",
+          `[Date].[Date].[Year].&[${election_year}]`
+        ]
+      },
+      "geo",
+      false
+    )
   ];
 
   render() {
     const { children, t, i18n } = this.props;
-    const { datum_electoral_participation, geo } = this.context.data;
+    const { datum_election_mayor, geo } = this.context.data;
 
     const locale = i18n.language;
-    const text = Election(datum_electoral_participation, geo, locale);
+    const text = undefined;
+    const text2 = textCivicsMayor(
+      geo,
+      datum_election_mayor,
+      election_year,
+      locale
+    );
+
+    if (text2) text2.position = t("mayor");
+
+    if (text2 && geo.depth === 2) {
+      const participation = this.context.data.need_mayor_participation.data[0];
+      text2.votes.participation = numeral(
+        participation.Votes / participation.Electors,
+        locale
+      ).format("0.0 %");
+    }
 
     return (
       <div className="topic-slide-block">
         <div className="topic-slide-intro">
           <div className="topic-slide-title">{t("Election")}</div>
-          <div className="topic-slide-text">
-            <p
-              dangerouslySetInnerHTML={{
-                __html: t("geo_profile.politics.text", text)
-              }}
-            />
-          </div>
+          <div
+            className="topic-slide-text"
+            dangerouslySetInnerHTML={{
+              __html: t("geo_profile.civics.mayor.text", text2)
+            }}
+          />
           <div className="topic-slide-data">
             {text && (
               <FeaturedDatum
