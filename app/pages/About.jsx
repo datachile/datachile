41a640545ee9bd887fb06c6e1d2e7c36bea7bd0c
Pablo H. Paladino <paladinop@gmail.com>
2018-05-11 12:17:55 -0300

    guayacan changelob

--- app/pages/About.jsx
+++ app/pages/About.jsx
@@ -850,6 +850,13 @@ class About extends Component {
               <section className="section" id="terms">
                 <h2>{t("about.changelog.title")}</h2>
 
+                <h3>{t("about.changelog.guayacan.title")}</h3>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: t("about.changelog.guayacan.description")
+                  }}
+                />
+
                 <h3>{t("about.changelog.fitzroya.title")}</h3>
                 <div
                   dangerouslySetInnerHTML={{
