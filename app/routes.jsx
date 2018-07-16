635c8bb569195a14e1db6301ab28dc3596dfea09
cnavarreteliz <cnavarreteliz@gmail.com>
2018-07-16 12:58:35 -0400

    enables map and explore map

--- app/routes.jsx
+++ app/routes.jsx
@@ -27,8 +27,8 @@ export default function RouteCreate() {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      {/*<Route path="explore/map/data" component={ExploreMapData} />*/}
-      {/*<Route path="explore/map" component={ExploreMap} />*/}
+      <Route path="explore/map/data" component={ExploreMapData} />
+      <Route path="explore/map" component={ExploreMap} />
       <Route path="explore(/:entity)(/:entity_id)" component={Explore} />
 
       <Route path="geo/:region(/:comuna)" component={GeoProfile} />
