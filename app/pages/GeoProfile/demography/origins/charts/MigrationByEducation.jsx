ce7752260ae069d57ff09c9859faa40c1f502943
cnavarreteliz <cnavarreteliz@gmail.com>
2018-10-17 08:58:41 -0300

    fixes total titles of migration section

--- app/pages/GeoProfile/demography/origins/charts/MigrationByEducation.jsx
+++ app/pages/GeoProfile/demography/origins/charts/MigrationByEducation.jsx
@@ -11,78 +11,74 @@ import SourceTooltip from "components/SourceTooltip";
 import ExportLink from "components/ExportLink";
 
 class MigrationByEducation extends Section {
-	static need = [
-		simpleGeoChartNeed(
-			"path_country_migration_by_education",
-			"immigration",
-			["Number of visas"],
-			{
-				drillDowns: [["Date", "Year"], ["Education", "Education", "Education"]],
-				options: { parents: true }
-			}
-		)
-	];
+  static need = [
+    simpleGeoChartNeed(
+      "path_country_migration_by_education",
+      "immigration",
+      ["Number of visas"],
+      {
+        drillDowns: [["Date", "Year"], ["Education", "Education", "Education"]],
+        options: { parents: true }
+      }
+    )
+  ];
 
-	render() {
-		const { t, className, i18n } = this.props;
+  render() {
+    const { t, className, i18n } = this.props;
 
-		const locale = i18n.language;
+    const locale = i18n.language;
 
-		const path = this.context.data.path_country_migration_by_education;
-		const classSvg = "migration-by-education";
+    const path = this.context.data.path_country_migration_by_education;
+    const classSvg = "migration-by-education";
 
-		return (
-			<div className={className}>
-				<h3 className="chart-title">
-					<span>
-						{t("Migration By Educational Level")}
-						<SourceTooltip cube="immigration" />
-					</span>
-					<ExportLink path={path} className={classSvg} />
-				</h3>
-				<TreemapStacked
-					className={classSvg}
-					path={path}
-					msrName="Number of visas"
-					drilldowns={["Education", "Education"]}
-					depth={true}
-					config={{
-						height: 400,
-						data: path,
-						label: d => d["Education"],
-						total: d => d["Number of visas"],
-						totalConfig: {
-							text: d =>
-								"Total: " +
-								numeral(d.text.split(": ")[1], locale).format("0,0") +
-								" " +
-								t("visas")
-						},
-						shapeConfig: {
-							fill: d =>
-								migrationByEducationColorScale("miged" + d["ID Education"])
-						},
-						tooltipConfig: {
-							title: d => d["Education"],
-							body: d =>
-								numeral(d["Number of visas"], locale).format("( 0,0 )") +
-								" " +
-								t("visas")
-						},
-						legend: false,
-						legendConfig: {
-							label: false,
-							shapeConfig: false
-						},
-						yConfig: {
-							title: t("Number of visas"),
-							tickFormat: tick => numeral(tick, locale).format("0,0")
-						}
-					}}
-				/>
-			</div>
-		);
-	}
+    return (
+      <div className={className}>
+        <h3 className="chart-title">
+          <span>
+            {t("Migration By Educational Level")}
+            <SourceTooltip cube="immigration" />
+          </span>
+          <ExportLink path={path} className={classSvg} />
+        </h3>
+        <TreemapStacked
+          className={classSvg}
+          path={path}
+          msrName="Number of visas"
+          drilldowns={["Education", "Education"]}
+          depth={true}
+          config={{
+            height: 400,
+            data: path,
+            label: d => d["Education"],
+            total: d => d["Number of visas"],
+            totalConfig: {
+              text: d => `${d.text} ${t("visas")}`
+            },
+            shapeConfig: {
+              fill: d =>
+                migrationByEducationColorScale("miged" + d["ID Education"])
+            },
+            tooltipConfig: {
+              title: d => d["Education"],
+              body: d =>
+                numeral(d["Number of visas"], locale).format("( 0,0 )") +
+                " " +
+                t("visas")
+            },
+            legend: false,
+            legendConfig: {
+              label: false,
+              shapeConfig: false
+            },
+            yConfig: {
+              title: t("Number of visas"),
+              tickFormat: tick => numeral(tick, locale).format("0,0")
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default translate()(MigrationByEducation);
--- app/pages/GeoProfile/demography/origins/charts/MigrationByVisa.jsx
+++ app/pages/GeoProfile/demography/origins/charts/MigrationByVisa.jsx
@@ -16,118 +16,118 @@ import ExportLink from "components/ExportLink";
 import SourceTooltip from "components/SourceTooltip";
 
 class MigrationByVisa extends Section {
-	// state = {
-	//   filter_sex: 2147483647,
-	//   filter_age: 2147483647
-	// };
+  // state = {
+  //   filter_sex: 2147483647,
+  //   filter_age: 2147483647
+  // };
 
-	static need = [
-		(params, store) => {
-			const geo = getGeoObject(params);
-			const promise = mondrianClient.cube("immigration").then(cube => {
-				const q = cube.query
-					.option("parents", true)
-					.drilldown("Date", "Date", "Year")
-					// .drilldown("Sex", "Sex", "Sex")
-					// .drilldown(
-					//   "Calculated Age Range",
-					//   "Calculated Age Range",
-					//   "Age Range"
-					// )
-					.drilldown("Origin Country", "Country", "Country")
-					.drilldown("Visa Type", "Visa Type", "Visa Type")
-					.measure("Number of visas");
+  static need = [
+    (params, store) => {
+      const geo = getGeoObject(params);
+      const promise = mondrianClient.cube("immigration").then(cube => {
+        const q = cube.query
+          .option("parents", true)
+          .drilldown("Date", "Date", "Year")
+          // .drilldown("Sex", "Sex", "Sex")
+          // .drilldown(
+          //   "Calculated Age Range",
+          //   "Calculated Age Range",
+          //   "Age Range"
+          // )
+          .drilldown("Origin Country", "Country", "Country")
+          .drilldown("Visa Type", "Visa Type", "Visa Type")
+          .measure("Number of visas");
 
-				// return mondrianClient.query(
-				//   geoCut(geo, "Geography", q, store.i18n.locale),
-				//   "jsonrecords"
-				// );
-				return {
-					key: "path_migration_by_visa_type",
-					data:
-						__API__ +
-						geoCut(geo, "Geography", q, store.i18n.locale).path("jsonrecords")
-				};
-			});
-			// .then(res => {
-			//   return {
-			//     key: "chart_migration_by_visa_type",
-			//     data: {
-			//       path: res.url.replace("aggregate", "aggregate.jsonrecords"),
-			//       raw: res.data.data,
-			//       age_ranges: Object.keys(groupBy(res.data.data, "Age Range")).sort(
-			//         (a, b) => parseInt(a.split("-")[0]) - parseInt(b.split("-")[0])
-			//       )
-			//     }
-			//   };
-			// });
+        // return mondrianClient.query(
+        //   geoCut(geo, "Geography", q, store.i18n.locale),
+        //   "jsonrecords"
+        // );
+        return {
+          key: "path_migration_by_visa_type",
+          data:
+            __API__ +
+            geoCut(geo, "Geography", q, store.i18n.locale).path("jsonrecords")
+        };
+      });
+      // .then(res => {
+      //   return {
+      //     key: "chart_migration_by_visa_type",
+      //     data: {
+      //       path: res.url.replace("aggregate", "aggregate.jsonrecords"),
+      //       raw: res.data.data,
+      //       age_ranges: Object.keys(groupBy(res.data.data, "Age Range")).sort(
+      //         (a, b) => parseInt(a.split("-")[0]) - parseInt(b.split("-")[0])
+      //       )
+      //     }
+      //   };
+      // });
 
-			return { type: "GET_DATA", promise };
-		}
-	];
+      return { type: "GET_DATA", promise };
+    }
+  ];
 
-	// toggleFilter = (key, flag) => {
-	//   this.setState(prevState => ({ [key]: prevState[key] ^ flag }));
-	// };
+  // toggleFilter = (key, flag) => {
+  //   this.setState(prevState => ({ [key]: prevState[key] ^ flag }));
+  // };
 
-	render() {
-		const { t, className, i18n } = this.props;
-		// const { filter_sex, filter_age } = this.state;
-		const locale = i18n.language;
+  render() {
+    const { t, className, i18n } = this.props;
+    // const { filter_sex, filter_age } = this.state;
+    const locale = i18n.language;
 
-		const chart_path = this.context.data.path_migration_by_visa_type;
+    const chart_path = this.context.data.path_migration_by_visa_type;
 
-		// const chart_data = this.context.data.chart_migration_by_visa_type;
+    // const chart_data = this.context.data.chart_migration_by_visa_type;
 
-		// const flags_ageranges = {};
+    // const flags_ageranges = {};
 
-		// const filters = [
-		//   {
-		//     name: t("Gender"),
-		//     key: "filter_sex",
-		//     value: filter_sex,
-		//     items: [{ label: t("Female"), flag: 1 }, { label: t("Male"), flag: 2 }]
-		//   },
-		//   {
-		//     name: t("Age"),
-		//     key: "filter_age",
-		//     value: filter_age,
-		//     items: chart_data.age_ranges.map((range, i) => {
-		//       flags_ageranges[range] = Math.pow(2, i);
-		//       return { label: range, flag: Math.pow(2, i) };
-		//     })
-		//   }
-		// ];
+    // const filters = [
+    //   {
+    //     name: t("Gender"),
+    //     key: "filter_sex",
+    //     value: filter_sex,
+    //     items: [{ label: t("Female"), flag: 1 }, { label: t("Male"), flag: 2 }]
+    //   },
+    //   {
+    //     name: t("Age"),
+    //     key: "filter_age",
+    //     value: filter_age,
+    //     items: chart_data.age_ranges.map((range, i) => {
+    //       flags_ageranges[range] = Math.pow(2, i);
+    //       return { label: range, flag: Math.pow(2, i) };
+    //     })
+    //   }
+    // ];
 
-		const classSvg = "migration-by-visa";
+    const classSvg = "migration-by-visa";
 
-		return (
-			<div className={className}>
-				<h3 className="chart-title">
-					<span>
-						{t("Migration By Visa Type")}
-						<SourceTooltip cube="immigration" />
-					</span>
-					<ExportLink path={chart_path} className={classSvg} />
-				</h3>
-				{/* <MiniFilter onClick={this.toggleFilter} filters={filters} /> */}
-				<Treemap
-					className={classSvg}
-					config={{
-						height: 400,
-						data: chart_path,
-						groupBy: ["ID Continent", "ID Visa Type"],
-						label: d => d["Visa Type"],
-						time: "ID Year",
-						//stacked: true,
-						sum: d => d["Number of visas"],
-						//x: "Visa Type",
-						shapeConfig: {
-							fill: d => continentColorScale("c" + d["ID Continent"]),
-							//label: false,
-							//height: 20
-						},
-						/*xConfig: {
+    return (
+      <div className={className}>
+        <h3 className="chart-title">
+          <span>
+            {t("Migration By Visa Type")}
+            <SourceTooltip cube="immigration" />
+          </span>
+          <ExportLink path={chart_path} className={classSvg} />
+        </h3>
+        {/* <MiniFilter onClick={this.toggleFilter} filters={filters} /> */}
+        <Treemap
+          className={classSvg}
+          config={{
+            height: 400,
+            data: chart_path,
+            groupBy: ["ID Continent", "ID Visa Type"],
+            label: d => d["Visa Type"],
+            time: "ID Year",
+            //stacked: true,
+            sum: d => d["Number of visas"],
+            //x: "Visa Type",
+            shapeConfig: {
+              fill: d => continentColorScale("c" + d["ID Continent"])
+              //label: false,
+              //height: 20
+            },
+            /*xConfig: {
 							tickSize: 0,
 							title: false
 						},
@@ -136,76 +136,70 @@ class MigrationByVisa extends Section {
 							tickSize: 0,
 							title: t("Number of visas")
 						},*/
-						//ySort: (a, b) => a["Number of visas"] - b["Number of visas"],
-						total: d => d["Number of visas"],
-						totalConfig: {
-							text: d =>
-								"Total: " +
-								numeral(getNumberFromTotalString(d.text), locale).format(
-									"(0,0)"
-								) +
-								" " +
-								"visas"
-						},
-						barPadding: 0,
-						groupPadding: 5,
-						tooltipConfig: {
-							title: d => d["Visa Type"] + " - " + d["Continent"],
-							body: d =>
-								t("{{number}} visas", {
-									number: numeral(d["Number of visas"], locale).format(
-										"( 0,0 )"
-									)
-								})
-						},
-						legendConfig: {
-							label: false,
-							shapeConfig: {
-								backgroundImage: d =>
-									"/images/legend/continent/" + d["ID Continent"] + ".png"
-							}
-						}
-					}}
-					dataFormat={function(data) {
-						const resp = data.data.filter(d => d["Number of visas"] > 0);
+            //ySort: (a, b) => a["Number of visas"] - b["Number of visas"],
+            total: d => d["Number of visas"],
+            totalConfig: {
+              text: d => `${d.text} ${t("visas")}`
+            },
+            barPadding: 0,
+            groupPadding: 5,
+            tooltipConfig: {
+              title: d => d["Visa Type"] + " - " + d["Continent"],
+              body: d =>
+                t("{{number}} visas", {
+                  number: numeral(d["Number of visas"], locale).format(
+                    "( 0,0 )"
+                  )
+                })
+            },
+            legendConfig: {
+              label: false,
+              shapeConfig: {
+                backgroundImage: d =>
+                  "/images/legend/continent/" + d["ID Continent"] + ".png"
+              }
+            }
+          }}
+          dataFormat={function(data) {
+            const resp = data.data.filter(d => d["Number of visas"] > 0);
 
-						if (!resp.length) return [];
+            if (!resp.length) return [];
 
-						let grouped = groupBy(resp, "ID Year");
-						return Object.keys(grouped).reduce(function(all, year) {
-							let children = orderBy(
-								grouped[year],
-								["Number of visas"],
-								["desc"]
-							);
+            let grouped = groupBy(resp, "ID Year");
+            return Object.keys(grouped).reduce(function(all, year) {
+              let children = orderBy(
+                grouped[year],
+                ["Number of visas"],
+                ["desc"]
+              );
 
-							if (children.length > 20) {
-								children = children.slice(0, 19).concat({
-									...children[0],
-									Country: t("Others"),
-									"Number of visas": children
-										.slice(19)
-										.reduce((sum, r) => sum + r["Number of visas"], 0)
-								});
-							}
+              if (children.length > 20) {
+                children = children.slice(0, 19).concat({
+                  ...children[0],
+                  Country: t("Others"),
+                  "Number of visas": children
+                    .slice(19)
+                    .reduce((sum, r) => sum + r["Number of visas"], 0)
+                });
+              }
 
-							return all.concat(children);
-						}, []);
+              return all.concat(children);
+            }, []);
 
-						// const filtered = data.filter(d => {
-						//   const age_range = flags_ageranges[d["Age Range"]];
-						//   return (
-						//     d["Number of visas"] > 0 &&
-						//     (filter_sex & d["ID Sex"]) == d["ID Sex"] &&
-						//     (filter_age & age_range) == age_range
-						//   );
-						// });
-						// return orderBy(filtered, ["Number of visas"], ["desc"]);
-					}}
-				/>
-			</div>
-		);
-	}
+            // const filtered = data.filter(d => {
+            //   const age_range = flags_ageranges[d["Age Range"]];
+            //   return (
+            //     d["Number of visas"] > 0 &&
+            //     (filter_sex & d["ID Sex"]) == d["ID Sex"] &&
+            //     (filter_age & age_range) == age_range
+            //   );
+            // });
+            // return orderBy(filtered, ["Number of visas"], ["desc"]);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default translate()(MigrationByVisa);
