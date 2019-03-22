11b4e1372cdc8260cbf8d510ee2b224889e50dc5
cnavarreteliz <cnavarreteliz@gmail.com>
2019-03-22 18:04:13 -0300

    updates package

--- canon.js
+++ canon.js
@@ -1,5 +1,5 @@
-module.exports = {
-  logiclayer: {
-
-  }
-};
+/**
+    The object that this file exports is used to set configurations for canon
+    and it's sub-modules.
+*/
+module.exports = {};
